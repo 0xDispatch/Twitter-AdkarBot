b2fdc90b2af6780bdd301c891c8bb26c8973c7a2
0xDispatch <76801659+0xDispatch@users.noreply.github.com>
2022-03-05 09:06:16 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -2,10 +2,11 @@ import json
 import random
 import time
 import tweepy
-consumer_key = 'L35E1SwBQnLfRalLKx6YK5I6B'
-consumer_secret = 'rtEh2fcOwC6eBlD0hefCaKVhtvf5LMb01gaAdJ33TGAP15aGfs'
-access_token= "1499774234129743877-jTS39Twb6vYQ7q8sBYTbShBo49HOQt"
-access_secret = "qVMMIsZnp0qHoMrdCEfAsbod9GJxkl6Q5GxHhPTykIhXF"
+#استبدل الاكس بالمفاتيح الخاصة بحسابك
+consumer_key = 'xxxxx'
+consumer_secret = 'xxxxx'
+access_token= "xxxxx"
+access_secret = "xxxxxx"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -13,25 +14,24 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 def main():
     with open("azkar.json", "r") as read_file:
         data = json.load(read_file)  
-    t = random.choice(data)
-    #print(t)
-    category = t["category"]
-    zekr = t["zekr"]
+    random_azkar = random.choice(data)
+    category = random_azkar["category"]
+    zekr = random_azkar["zekr"]
     l = len(zekr)
     print(l)
+#في حالة ان الذكر اكثر من ١٦٠ حرف  يتم الاختيار مره اخرى لان تويتر لا يقبل اكثر من ١٨٠ حرف
     while l > 160:
-        t = random.choice(data)
-        category = t["category"]
-        zekr = t["zekr"]
+        random_azkar = random.choice(data)
+        category = random_azkar["category"]
+        zekr = random_azkar["zekr"]
         l = len(zekr)
         print(l)
-    #print(category)
-    #print(zekr)
-    print("التصنيف : "+category+"\nالذكر : "+zekr+"\n")
-    api.update_status("التصنيف : "+category+"\nالذكر : "+zekr+"\n#اذكار")
+    print("التصنيف: "+category+"\nالذكر: "+zekr+"\n")
+    #نشر الذكر في تويتر
+    api.update_status("التصنيف: "+category+"\nالذكر: "+zekr+"\n#اذكار")
     print("Tweeted")
    
-
+#تكرار مع انتظار ١٨٠٠ ثانية يساوي نصف ساعة
 while True:
     main()
     time.sleep(1800)
