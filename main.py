1bdb60de5bbe07d3935c1d4b533618e8e04ae2f9
Faris <76801659+0xDispatch@users.noreply.github.com>
2022-07-29 16:12:29 +0300

    Update main.py
    1 -  تعديل صيغة التغريدة
    2 - تحويل التصنيف الى هاشتاق
    3 - بعض التعليقات الأرشادية {:

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import random
 import time
 import tweepy
 #استبدل الاكس بالمفاتيح الخاصة بحسابك
-consumer_key = 'xxxxx'
+#تجيب المفاتيح من هنا
+#developer.twitter.com/apps
+consumer_key = 'xxxxxxx'
 consumer_secret = 'xxxxx'
 access_token= "xxxxx"
 access_secret = "xxxxxx"
+
+#مصادقة لمكتبة tweepy مع twitter api ما يحتاج تغير فيها الا لو بتستعمل api v2
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
 
 def main():
+    #فتح ملف الأذكار
     with open("azkar.json", "r") as read_file:
-        data = json.load(read_file)  
+        data = json.load(read_file)
+    #اختيار ذِكر عشوائي
     random_azkar = random.choice(data)
     category = random_azkar["category"]
     zekr = random_azkar["zekr"]
+    #عدد حروف الذِكر
     l = len(zekr)
     print(l)
 #في حالة ان الذكر اكثر من ١٦٠ حرف  يتم الاختيار مره اخرى لان تويتر لا يقبل اكثر من ١٨٠ حرف
@@ -26,10 +35,13 @@ def main():
         zekr = random_azkar["zekr"]
         l = len(zekr)
         print(l)
-    print("التصنيف: "+category+"\nالذكر: "+zekr+"\n")
+    #استبدال المسافه بـ _ 
+    category_hashtag = category.replace(" ", "_")
+    #صيغة التغريدة النهائية
+    fulltweet = f"{zekr}\n\n#{category_hashtag}\n#اذكار"
     #نشر الذكر في تويتر
-    api.update_status("التصنيف: "+category+"\nالذكر: "+zekr+"\n#اذكار")
-    print("Tweeted")
+    api.update_status(fulltweet)
+    print("تم التغريد\nالتغريدة : {fulltweet}\nانتظار نصف ساعة...")
    
 #تكرار مع انتظار ١٨٠٠ ثانية يساوي نصف ساعة
 while True:
