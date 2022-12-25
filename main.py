b5185fba640850adc568f84d0dc0f467ca2a9b9e
Faris <76801659+0xDispatch@users.noreply.github.com>
2022-12-25 21:10:01 +0300

    تحديث البوت
    تحسين الكود و اضافه توقيت للأذكار
    كل صباح بينزل اذكار الصباح وكل مساء اذكار المساء و يوم الجمعة سورة الكهف

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import random
 import time
 import tweepy
+from pytz import timezone
+from datetime import datetime
 #استبدل الاكس بالمفاتيح الخاصة بحسابك
 #تجيب المفاتيح من هنا
 #developer.twitter.com/apps
@@ -22,26 +24,37 @@ def main():
     with open("azkar.json", "r") as read_file:
         data = json.load(read_file)
     #اختيار ذِكر عشوائي
-    random_azkar = random.choice(data)
+    morning = ["02", "03", "04", "05"]
+    night = ["15", "16", "17", "18"]
+    types = list(data.keys())
+    now_utc = datetime.now(timezone('UTC'))
+    time = now_utc.strftime("%H")
+    day = now_utc.strftime("%A")
+    if time in morning:
+        type = "أذكار الصباح"
+    elif time in night:
+        type = "أذكار المساء"
+    elif time == "09" and day.lower() == "friday":
+        print("يوم الجمعة، جار تغريد سورة الكهف")
+        api.update_status("‏‎#سورة_الكهف ‎#يوم_الجمعه pic.twitter.com/7TvzlEoQhj‎‎‎")
+    else:
+        type = random.choice(['تسابيح', 'أدعية قرآنية', 'أدعية الأنبياء'])
+    random_azkar = random.choice(data[types[types.index(type)]])
     category = random_azkar["category"]
-    zekr = random_azkar["zekr"]
+    zekr = random_azkar["content"]
     #عدد حروف الذِكر
     l = len(zekr)
     print(l)
 #في حالة ان الذكر اكثر من ٢٦٠ حرف  يتم الاختيار مره اخرى لان تويتر لا يقبل اكثر من ٢٨٠ حرف
-    while l > 260:
-        random_azkar = random.choice(data)
-        category = random_azkar["category"]
-        zekr = random_azkar["zekr"]
-        l = len(zekr)
-        print(l)
+    if l > 280:
+        return main()
     #استبدال المسافه بـ _ 
     category_hashtag = category.replace(" ", "_")
     #صيغة التغريدة النهائية
     fulltweet = f"{zekr}\n\n#{category_hashtag}\n#اذكار"
     #نشر الذكر في تويتر
     api.update_status(fulltweet)
-    print("تم التغريد\nالتغريدة : {fulltweet}\nانتظار نصف ساعة...")
+    print(f"تم التغريد\nالتغريدة : {fulltweet}\nانتظار نصف ساعة...")
    
 #تكرار مع انتظار ١٨٠٠ ثانية يساوي نصف ساعة
 while True:
